e2043f3ff99ae947ec5c5a48091d084413196044
taogeYT <li_yatao@outlook.com>
2017-10-25 21:20:51 +0800

    update

--- install.bat
+++ install.bat
@@ -1,5 +1,6 @@
-rd /q /s py_db-0.0.6
-WinRAR.exe x -ad py_db-0.0.6.tar.gz
-cd py_db-0.0.6\py_db-0.0.6
+rd /q /s dist
+python setup.py sdist
+WinRAR.exe x -d dist\*.tar.gz dist
+cd dist\py_db-0.0.6
 python setup.py install
 pause
