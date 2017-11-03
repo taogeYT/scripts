28ba12828467598dde230e7b350c5810e00053a3
liyt@vastio.com <liyt@vastio.com>
2017-11-03 09:03:29 +0800

    update

--- install.bat
+++ install.bat
@@ -1,6 +1,8 @@
 rd /q /s dist
 python setup.py sdist
 ..\WinRAR.exe x -d dist\*.tar.gz dist
-cd dist\py_db-0.0.7
+cd dist
+::for /f "delims=" %%a in ('dir /b/a-d/oN *.*') do  %%a
+for /f "delims=" %%a in ('dir /b/ad') do cd %%a
 python setup.py install
 pause
