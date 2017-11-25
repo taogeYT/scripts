00fb6464bf689af0b91b403157c89379a9637c81
taogeYT <li_yatao@outlook.com>
2017-11-25 16:25:21 +0800

    update

--- 设置代理.bat
+++ 设置代理.bat
@@ -3,7 +3,7 @@ echo 开始设置proxy代理..........
 echo 现在程序将关闭您的浏览器。。。。。
 taskkill /f /im iexplore.exe
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "127.0.0.1:80" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "192.168.3.2:8888" /f
 color 2
 echo 已成功设置代理服务器上网
 echo 按任意键关闭此对话框
