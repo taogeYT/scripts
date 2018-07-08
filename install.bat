0410ab233e604bd335358214ddf54e4b1694b860
taogeYT <li_yatao@outlook.com>
2018-07-08 21:16:21 +0800

    python安装包一键安装

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 rd /q /s dist
 python setup.py sdist
-..\WinRAR.exe x -d dist\*.tar.gz dist
+.\WinRAR.exe x -d dist\*.tar.gz dist
 cd dist
 ::for /f "delims=" %%a in ('dir /b/a-d/oN *.*') do  %%a
 for /f "delims=" %%a in ('dir /b/ad') do cd %%a
